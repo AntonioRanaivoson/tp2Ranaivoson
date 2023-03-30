1693b3fa020de4d562cee291fbc9658e728768a0
Antonio Ranaivoson <tonioranaivo10@gmail.com>
2023-03-30 15:08:27 +0300

    TP2 Exercice 3

--- src/main/java/mg/ranaivoson/tp2ranaivoson/jsf/Bean.java
+++ src/main/java/mg/ranaivoson/tp2ranaivoson/jsf/Bean.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
+
 /**
  *
  * @author Antonio
@@ -16,7 +17,7 @@ import jakarta.enterprise.context.RequestScoped;
 @RequestScoped
 public class Bean {
 
-   private int nombre;
+    private int nombre;
 
     public int getNombre() {
         return nombre;
@@ -34,5 +35,9 @@ public class Bean {
         }
         return l;
     }
-    
+
+    public String afficher() {
+        return "affichage_3?nb=" + nombre + "&amp;faces-redirect=true";
+    }
+
 }
